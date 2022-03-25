c086227ccd6bf7716e626f3b8749a853ba2fc89d
JelleVisser01 <jellevisser.01@gmail.com>
2022-03-25 16:37:17 +0100

    Update ShootController.cs

--- Assets/Scripts/ShootController.cs
+++ Assets/Scripts/ShootController.cs
@@ -7,9 +7,16 @@ public class ShootController : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab = null;
     [SerializeField] private Transform aimedTransform;
     public Vector3 direction;
-    
-     
+
+
     private Camera mainCamera;
+    public enum GunType { Semi, Auto, Sniper };
+    public GunType gunType;
+
+    public float bulletSpeed = 12.0f;
+    public float timeBetweenShots;
+    private float nextPossibleShootTime;
+
 
     void Start()
     {
@@ -20,8 +27,26 @@ public class ShootController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (gunType == GunType.Semi)
+        {
+            timeBetweenShots = 0.5f;
+            bulletSpeed = 18.0f;
+        }
+        else if (gunType == GunType.Auto)
+        {
+            timeBetweenShots = 0.25f;
+            bulletSpeed = 14.0f;
+        }
+        else if (gunType == GunType.Sniper)
+        {
+            timeBetweenShots = 1.0f;
+            bulletSpeed = 35.0f;
+        }
         Aim();
-        Shoot();
+        if (Input.GetMouseButtonDown(0))
+            Shoot();
+        else if (Input.GetMouseButton(0))
+            ShootContinuous();
     }
 
     private void Aim()
@@ -59,12 +84,41 @@ public class ShootController : MonoBehaviour
 
     private void Shoot()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (CanShoot())
         {
-            var projectile = Instantiate(projectilePrefab, transform.position + Vector3.Scale(direction.normalized,  new Vector3(1.0f,1.0f,1.0f)), Quaternion.identity);
-            var rb = projectile.GetComponent<Rigidbody>();
-            rb.velocity = Vector3.Scale(direction.normalized, new Vector3(8.0f,8.0f,8.0f));
+            if (gunType == GunType.Sniper)
+            {
+                var projectile = Instantiate(projectilePrefab, transform.position + Vector3.Scale(direction.normalized, new Vector3(1.0f, 1.0f, 1.0f)), Quaternion.identity);
+                var rb = projectile.GetComponent<Rigidbody>();
+                rb.velocity = Vector3.Scale(direction.normalized, new Vector3(bulletSpeed, bulletSpeed, bulletSpeed));
+
+            }
+            else
+            {
+                var projectile = Instantiate(projectilePrefab, transform.position + Vector3.Scale(direction.normalized, new Vector3(1.0f, 1.0f, 1.0f)), Quaternion.identity);
+                var rb = projectile.GetComponent<Rigidbody>();
+                rb.velocity = Vector3.Scale(direction.normalized, new Vector3(bulletSpeed, bulletSpeed, bulletSpeed));
+            }
+
+            nextPossibleShootTime = Time.time + timeBetweenShots;
         }
     }
 
+    private void ShootContinuous()
+    {
+        if (gunType == GunType.Auto)
+            Shoot();
+    }
+
+    private bool CanShoot()
+    {
+        bool canShoot = true;
+
+        if (Time.time < nextPossibleShootTime)
+            canShoot = false;
+
+        return canShoot;
+    }
+
 }
+
